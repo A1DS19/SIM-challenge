959a5a61899d6b176b6672ad75ae7a77fb959349
Jose Enrique Padilla <jp06171@gmail.com>
2024-08-28 14:13:22 -0600

    add result to day_1

--- advent-of-code/day_1/src/main.rs
+++ advent-of-code/day_1/src/main.rs
@@ -2,6 +2,6 @@ mod challenges;
 
 use challenges::{part_1::part_1::result_part_1, part_2::part_2::result_part_2};
 fn main() {
-    result_part_1().unwrap();
-    result_part_2().unwrap();
+    result_part_1().unwrap(); // 54239
+    result_part_2().unwrap(); // 55343
 }
